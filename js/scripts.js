7834d5f554d7520988f1943d5d2ccf446939f2f7
mellwood1 <ellwoodmatthew04@gmail.com>
2026-03-03 13:55:24 -0500

    Forest

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 // When the user scrolls down 800px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
-    document.getElementById("cornerbtn").style.display = "block";
-} else {
-    document.getElementById("cornerbtn").style.display = "none";
-}
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("cornerbtn").style.display = "block";
+    } else {
+        document.getElementById("cornerbtn").style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+ 
+window.onload = function() {
+    this.document.getElementById("copyrightyear").innerHTML = new Date().getFullYear();
 }
